539496f1a0256dfa116e33ce155b54ad5c72e275
chanjl <chanjl@i2r.a-star.edu.sg>
2021-10-07 23:08:07 +0800

    add header file

---
+++ include/ros2_learning/talker.hpp
@@ -0,0 +1,35 @@
+#ifndef TALKER_H__
+#define TALKER_H__
+
+// ROS2
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp/executors.hpp>
+
+// msgs
+#include <std_msgs/msg/int64.hpp>
+
+// Cpp and STL
+#include <chrono>
+#include <string>
+#include <memory>
+#include <functional>
+
+namespace ros_learning
+{
+    class talker : public rclcpp::Node
+    {
+    public:
+        talker();
+        talker(const std::string &);
+        ~talker();
+        void run();
+    private:
+        size_t count_;
+        rclcpp::Publisher<std_msgs::msg::Int64>::SharedPtr pub_;
+    };
+
+
+} // namespace ros_learning
+
+
+#endif
