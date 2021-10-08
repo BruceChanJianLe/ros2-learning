865739b7f0023fa07ef8266f53c7f566474a05bf
chanjl <chanjl@i2r.a-star.edu.sg>
2021-10-08 13:24:50 +0800

    add talker2 source files

---
+++ src/talker2.cpp
@@ -0,0 +1,60 @@
+#include "ros2_learning/talker2.hpp"
+
+namespace ros_learning
+{
+    talker::talker()
+    : Node("talker_node")
+    ,   count_(0)
+    {
+        pub_ = this->create_publisher<std_msgs::msg::Int64>("talker/channel", 1);
+
+        timer_ = this->create_wall_timer(
+            std::chrono::seconds (1),
+            [this]()
+            {
+                this->run();
+            }
+        );
+    }
+
+    talker::talker(const std::string & node_name)
+    :   Node(node_name)
+    ,   count_(0)
+    {
+        pub_ = this->create_publisher<std_msgs::msg::Int64>("talker/channel", 1);
+
+        timer_ = this->create_wall_timer(
+            std::chrono::seconds (1),
+            [this]()
+            {
+                this->run();
+            }
+        );
+    }
+
+    talker::~talker()
+    {
+    }
+
+    void talker::run()
+    {
+        // Rate of publish
+        rclcpp::WallRate r(1);
+
+        // Prepare msg
+        auto msg = std_msgs::msg::Int64();
+        msg.data = 0;
+
+        while(rclcpp::ok())
+        {
+            pub_->publish(msg);
+            ++msg.data;
+
+            // Print in terminal
+            RCLCPP_INFO(this->get_logger(), "I published %i", msg.data);
+
+            r.sleep();
+        }
+
+    }
+} // namespace ros_learning
